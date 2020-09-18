511979ede2f6c13f79e6c489a4b44ca21ef358f5
dmoftakhar <david@chillaweb.com>
2020-09-18 14:27:26 +0100

    Create new homepage layout.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,9 +5,9 @@
 <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
     <main role="main" class="inner cover">
       <h1 class="cover-heading">My Workout Planner</h1>
-      <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+      <p class="lead">Build your own workout plan and use the power of AI to get the best results from your exercise regime.</p>
       <p class="lead">
-        <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        <a href="#" class="btn btn-primary btn-lg">Sign Up</a>
       </p>
     </main>
 
