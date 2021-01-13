5523efc0f2418c66d14478ef45895ff15b775281
DavidMoftakhar <david@chillaweb.com>
2021-01-13 17:34:40 +0000

    Controller for individual exercises.

--- app/Exercise.php
+++ app/Exercise.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Exercise extends Model
 {
     protected $fillable = ['exerciseName', 'exerciseCategory'];
+
+    public function routines()
+    {
+        return $this->belongsToMany(Routine::class)->withTimestamps();
+    }
 }
